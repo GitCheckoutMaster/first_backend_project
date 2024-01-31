4e8e938190dd34b1e42f9350fb656006017562f0
MrPrince <jaymistry31170@gmail.com>
2024-02-01 00:17:55 +0530

    A nightmare to understand and i still dont got it, it is about util functions and error

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
-        console.log(`MongoDB connected!! DB HOST: ${connectionInstance}`);
+        console.log(`MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("mongodb connection error ", error);
         process.exit(1);
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+            next(error)
+        );
+    };
+};
+
+export { asyncHandler };
+
+// another way
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
